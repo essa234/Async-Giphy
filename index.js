8f2fab75fff06497ab01659e8449e1dc671b5c03
Essa Essa <essa.essa@transferwise.com>
2023-05-02 15:17:21 +0100

    v1

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+//const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`;
+const endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=Pp9HJmT34iPHhewG9IxCOjGv7FwL5i4q&q=dogs&limit=25&offset=0&rating=g&lang=en';
+
+
+async function getImage(query){
+    const response = await fetch(endpoint);
+    //const data = await JSON.parse(response);
+    const data = await response.json();
+    console.log(data);
+    let newurl = 'https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=' + query + '&limit=25&offset=0&rating=g&lang=en';
+    return newurl;
+}
+
+
